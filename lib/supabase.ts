cedc91d02a36ec77cc2e9ec305064dc84e37b556
alfre2000bui <alfre2000bui@gmail.com>
2025-04-19 18:12:29 +0000

    update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,16 @@
 import { AppState } from 'react-native'
 import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import Constants from 'expo-constants'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_REACT_NATIVE_SUPABASE_URL'
-const supabaseAnonKey = 'YOUR_REACT_NATIVE_SUPABASE_ANON_KEY'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+
+
+const supabaseUrl = Constants.expoConfig?.extra?.SUPABASE_URL
+const supabaseAnonKey = Constants.expoConfig?.extra?.SUPABASE_KEY
+
+export const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
